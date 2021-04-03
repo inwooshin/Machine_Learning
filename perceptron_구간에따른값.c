1177b3e087e80b4cdd121248dfce5913cae2ddd2
inwooshin <60161818@mju.ac.kr>
2021-04-03 19:07:06 +0900

    구간에 따른 값
     - y의 값을 볼 수 있도록 출력한다.
     - 프린트를 하는데 y축의 값이 변하면 값을 띄우도록 한다.

--- perceptron_구간에따른값.c
+++ perceptron_구간에따른값.c
@@ -8,7 +8,8 @@ double before = 0;
 int perceptron(double x1, double x2) {
 	int y = 0;
 	if (x1 + x2 > 0.1) y = 1;
-
+	if (before != x2) { before = x2; printf("\n"); }
+	
 	// fabs 는 절댓값을 나타내는 함수이다.
 	// x1 와 x2 를 더한 값에서 0.1을 뺀 값이 같다고 해도 0.0 으로 떨어지는 것이 아닌
 	// 0.000000000000000056 같이 쓰레기 값을 남긴다. 
@@ -18,7 +19,7 @@ int perceptron(double x1, double x2) {
 	// 결과 값이 0.000001 보다 작은 경우에는 같은 것으로 규정한다.
 	// 그 반대는 크거나 같은 경우인데, 이러한 경우는 같지 않은 것이다.
 	if (fabs(x1 + x2 - 0.1) < Min) y = 1;
-
+	printf("%d  ", y);
 	return y;
 }
 
