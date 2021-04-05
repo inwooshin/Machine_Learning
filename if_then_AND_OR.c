6aacabc22330d73dff9e5290574fd72ad22a435e
inwooshin <60161818@mju.ac.kr>
2021-04-05 16:59:09 +0900

    if then 방식으로 and_or 로 weight 값 조정 (perceptron)
    w1, w2, delta 를 넣어주면 x1,x2 를 각각 00 01 10 11 을 주고
    w1 * x1 + w2 * x2 가 delta 를 넘으면 결과값인 y를 1, 넘지 않으면 0 으로 해서
    and 의 기대값 0 0 0 1 과 같도록 weight 값을 조정한다.
    or 로 하면 기대값 0 1 1 1 과 같도록 weight 값을 조정한다.
    
     - weight 값의 조정은 기대값이 1 일 경우 y 값이 0 이라면 weight 에 1.5 배를 해준다.
     - 기대값이 0 일 경우 y 값이 1 이면 weight 값에 0.5를 곱해준다.

---
+++ if_then_AND_OR.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <math.h>
+
+float w1 = 0, w2 = 0, thres = 0;
+float before = 0;
+
+int AND_OR(int x1, int x2, int t[], int i) {
+	int result = 0;
+	
+	if (w1 * x1 + w2 * x2 > thres || fabs(w1 * x1 + w2 * x2 - thres) < 0.000001) result = 1;
+	else result = 0;
+
+	if (result == t[i]) return 1;
+	else if (result == 1) {
+		if (x1 != 0) w1 /= 2;
+		if (x2 != 0) w2 /= 2;
+	}
+	else if (result == 0) {
+		if (x1 != 0) w1 *= 1.5;
+		if (x2 != 0) w2 *= 1.5;
+	}
+
+	return 0;
+}
+
+int PlayPerceptron() {
+	int result = 0;
+
+	for (float x2 = 2; x2 >= -2.000001; x2 -= 0.1) {
+		for (float x1 = -2; x1 <= 2.000001; x1 += 0.1) {
+			result = 0;
+			if (before != x2) { before = x2; printf("\n"); }
+			if (w1 * x1 + w2 * x2 >= thres) result = 1;
+			printf("%d  ", result);
+		}
+	}
+
+	return result;
+}
+
+int main(void) {
+
+	int check = 0, count = 0, and_or = 0;
+
+	printf("w1, w2, threshold의 값을 입력하시오 : ");
+	scanf("%f %f %f", &w1, &w2, &thres);
+	
+	printf("OR(0) 혹은 AND(1) 혹은 EX-OR(2) 연산을 선택하세요 : ");
+	scanf("%d", &and_or);
+
+	int t[3][4] = { {0,1,1,1}, {0,0,0,1}, {0,1,1,0} }; // OR, AND, EX-OR logic
+	int x[4][2] = { {0,0}, {0,1}, {1,0}, {1,1} }; // x1, x2 입력
+	
+	printf("\n입력 좌표 : %f * x1 + %f * x2 = %f", w1, w2, thres);
+
+	if (and_or == 0) printf("\n\nOR 연산 시작\n");
+	else if(and_or == 1) printf("\n\nAND 연산 시작\n");
+	else if(and_or == 2) printf("\n\nAND 연산 시작\n");
+	
+	while (check != 4) {
+		check = 0;
+		for (int i = 0; i < 4; i++) {
+			check += AND_OR(x[i][0], x[i][1], t[and_or], i);
+		}
+		count++;
+		if (check == 4) {
+			if (and_or == 0) { 
+				printf("\nOR 성립!"); 
+				printf("\nw1 = %f, w2 = %f, thres = %f", w1, w2, thres);
+				printf("\nOR로 변환한 좌표 : "); 
+			}
+			else {
+				printf("\nAND 성립");
+				printf("\nw1 = %f, w2 = %f, thres = %f", w1, w2, thres);
+				printf("\nAND로 변환한 좌표 : ");
+			}
+
+			printf("%f * x1 + %f * x2 = %f", w1, w2, thres);
+		}
+		if (count >= 100) {
+			printf("연산에 실패했습니다.");
+			break;
+		}
+	}
+
+	PlayPerceptron();
+
+	return 0;
+}
